45530dd6c5c82ec1ec6bb12e54456360943b7e8c
ALMaclaine <almaclaine@gmail.com>
2020-08-03 13:02:58 -0700

    finish initial 21_react_geolocation project

---
+++ 21_react_geolocation/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Compass from "./Compass";
+
+function App(props) {
+    return <Compass />
+}
+
+export default App;
---
+++ 21_react_geolocation/src/Speed.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react'
+import './Speed.css'
+
+function Speed(props) {
+    const [speed, setSpeed] = useState(0);
+    const { setHeading } = props;
+
+    useEffect(() => {
+        const navigatorHandler = (speed, heading) => {
+            setSpeed(speed);
+            setHeading(heading);
+        };
+
+        navigator.geolocation.watchPosition((data) => {
+            navigatorHandler(data.coords.speed, data.coords.heading);
+        }, (err) => {
+            console.error(err);
+        });
+    }, [setHeading]);
+
+    return <h1 className="speed">
+        <span className="speed-value">{speed}</span>
+        <span className="units">KM/H</span>
+    </h1>;
+}
+
+export default Speed;
