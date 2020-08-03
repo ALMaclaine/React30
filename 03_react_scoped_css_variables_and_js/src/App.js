00e428aa6207ed7db0788d6eac950c63415b329d
ALMaclaine <almaclaine@gmail.com>
2020-08-03 13:17:43 -0700

    update 03_react_scoped_css_variables_and_js component structure

--- 03_react_scoped_css_variables_and_js/src/App.js
+++ 03_react_scoped_css_variables_and_js/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import './App.css'
 import Controls from "./Controls";
+import CssVariables from "./CssVariables";
 
 function App(props) {
-    return (<>
-            <h2>Update CSS Variables with <span className='hl'>JS</span></h2>
-            <Controls />
-            <img alt={"To be affected"} src="https://source.unsplash.com/7bwQXzbF6KE/800x500" />
-        </>);
+    return <CssVariables />;
 }
 
 export default App;
---
+++ 03_react_scoped_css_variables_and_js/src/CssVariables.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Controls from "./Controls";
+
+function CssVariables(props) {
+    return <>
+        <h2>Update CSS Variables with <span className='hl'>JS</span></h2>
+        <Controls />
+        <img alt={"To be affected"} src="https://source.unsplash.com/7bwQXzbF6KE/800x500" />
+    </>;
+}
+
+export default CssVariables;
