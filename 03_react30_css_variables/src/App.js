091d3eb94ba16faffdee081007ad5b81ceb7d147
ALMaclaine <almaclaine@gmail.com>
2020-08-07 00:57:14 -0700

    small updates to 03_react30_css_variables

--- 03_react30_css_variables/src/App.js
+++ 03_react30_css_variables/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import CssVariables from "./CssVariables";
 
 function App(props) {
--- 03_react30_css_variables/src/CssVariables.js
+++ 03_react30_css_variables/src/CssVariables.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CssVariables.css';
 import Controls from "./Controls";
 
 function CssVariables(props) {
