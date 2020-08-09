66d972c42b3d7430b11622b318064ac11479c36f
ALMaclaine <almaclaine@gmail.com>
2020-08-09 15:20:05 -0700

    05 clean up

--- 05_react30_flex_panel_gallery/src/Panel.js
+++ 05_react30_flex_panel_gallery/src/Panel.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './Panel.css';
-import {CSSTransition} from "react-transition-group"
+import {CSSTransition} from 'react-transition-group';
 
 function Panel(props) {
     const {children, className} = props;
@@ -13,7 +13,7 @@ function Panel(props) {
     };
     const classNames = {enter: 'open', enterDone: 'open-active', exit: 'close-active'}
 
-    const finalClassName = "panel " + className;
+    const finalClassName = 'panel' + (className ? ` ${className}` : '');
     return <CSSTransition in={open}
                           timeout={timeout}
                           classNames={classNames}
